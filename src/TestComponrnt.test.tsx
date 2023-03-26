e0f4b0879ddc2ebca8ed3e7f8268b23d1a0f8fd6
kosukekashiwa <hanjuku.kabocha49@gmail.com>
2023-03-26 21:56:29 +0900

    MSW Test

--- src/TestComponrnt.test.tsx
+++ src/TestComponrnt.test.tsx
@@ -1,11 +1,52 @@
-import { render, screen } from '@testing-library/react'
+import { act, render, screen } from '@testing-library/react'
+import { rest } from 'msw'
+import { SWRConfig } from 'swr'
 
+import { server } from '~/mocks/server'
 import TestComponrnt from '~/TestComponrnt'
 
-test('「Hello Test」が描画されている', () => {
-  render(<TestComponrnt />)
+describe('Initial Rendering', () => {
+  test('「Hello World」が表示される', async () => {
+    await act(() => {
+      render(<TestComponrnt />)
+    })
 
-  screen.debug()
+    expect(screen.getByText('Hello World')).toBeInTheDocument()
+  })
 
-  expect(screen.getByText('Hello Test')).toBeInTheDocument()
+  test('MSW Sample', async () => {
+    await act(() => {
+      render(<TestComponrnt />)
+    })
+
+    expect(await screen.getByText('name-0')).toBeInTheDocument()
+    expect(await screen.getByText('name-1')).toBeInTheDocument()
+    expect(await screen.getByText('name-2')).toBeInTheDocument()
+  })
+
+  test('MSW One-time override', async () => {
+    server.use(
+      rest.get(`/api/v1/users`, (req, res, ctx) => {
+        return res.once(
+          ctx.status(200),
+          ctx.json([
+            { id: '3', name: 'name-3' },
+            { id: '4', name: 'name-4' },
+          ]),
+        )
+      }),
+    )
+
+    await act(() => {
+      render(
+        <SWRConfig value={{ provider: () => new Map() }}>
+          <TestComponrnt />
+        </SWRConfig>,
+      )
+    })
+
+    // expect(await screen.findByText('name-0')).toBeInTheDocument()
+    expect(await screen.getByText('name-3')).toBeInTheDocument()
+    expect(await screen.getByText('name-4')).toBeInTheDocument()
+  })
 })
--- src/TestComponrnt.tsx
+++ src/TestComponrnt.tsx
@@ -1,8 +1,17 @@
+import { useFetchUsers } from '~/state/user/apis'
+import { User } from '~/state/user/models'
+
 const TestComponrnt: React.FC = () => {
-  const title = 'Hello Test'
+  const { users } = useFetchUsers()
+
+  const title = 'Hello World'
+
   return (
     <div>
       <p>{title}</p>
+      {users?.map((item: User) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
     </div>
   )
 }
